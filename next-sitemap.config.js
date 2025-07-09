4104177c956375521f2b5a3b9c283082a17150de
Liam Scott <liamscott@Liams-MacBook-Air.local>
2025-07-09 20:08:38 +0800

    Update Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   siteUrl: 'https://thenomasclub.com',
   generateRobotsTxt: true,
+  additionalPaths: async (config) => [
+    await config.transform(config, '/builder'), // manually include homepage
+  ],
 };
